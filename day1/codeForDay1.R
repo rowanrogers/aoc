138642b7cf63728dc524381349d9c820a624f9ac
Rowan Rogers <rowan.rogers@hymans.co.uk>
2022-12-02 11:36:22 +0000

    Helpful notes

--- day1/codeForDay1.R
+++ day1/codeForDay1.R
@@ -1,11 +1,16 @@
 library(magrittr)
 
+
+## load helper functions for fetching data and parsing
 purrr::walk(paste0("R/",list.files("R")), source)
 
+# specify folder
 folder <- "day1"
 
+# load input from website
 input <- getInput(1,2022)
 
+# mutate to define which calorie count is related to each elf
 naCalc <-
   input %>%
   dplyr::rowwise() %>%
@@ -13,11 +18,14 @@ naCalc <-
   dplyr::ungroup() %>%
   dplyr::mutate(elfNumber = cumsum(.data$naCount) + 1)
 
+# sum over the calories for each elf
 calCount <-
   naCalc %>%
   dplyr::group_by(.data$elfNumber) %>%
   dplyr::summarise(calorieCount = sum(.data$x, na.rm = TRUE)) %>%
   dplyr::arrange(dplyr::desc(.data$calorieCount))
 
-
+# find max calorie count
+max(calCount$calorieCount)
+# find sum of top 3 calorie counts
 sum(calCount$calorieCount[1:3])
